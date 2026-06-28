b9606c0bb469b96e67cdf3c65e0a64e7e9370d84
Mehdi <mehdicontacto@gmail.com>
2026-06-28 12:02:20 +0300

    Task 2 done

---
+++ TypeScript/task2/js2.js
@@ -0,0 +1,5 @@
+"use strict";
+function print(name, occ, wage) {
+    return `Hey, ${name}. You are a(n) ${occ} and your hourly wage is ${wage} dollars.`;
+}
+console.log(print("Sam", "Engineer", 25));
---
+++ TypeScript/task2/js2.ts
@@ -0,0 +1,6 @@
+
+function print(name: string, occ: string, wage: number) {
+    return `Hey, ${name}. You are a(n) ${occ} and your hourly wage is ${wage} dollars.`
+}
+
+console.log(print("Sam", "Engineer", 25))
