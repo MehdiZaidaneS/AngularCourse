6eac0f8897060ff87351a5e3af18a3454f405e13
Mehdi <mehdicontacto@gmail.com>
2026-07-05 17:31:58 +0300

    Angular Basics Task 7 done

--- Angular Basics/new-project/src/app/app.ts
+++ Angular Basics/new-project/src/app/app.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [NgIf],
+  imports: [NgIf, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   active = false;
+  message: string = '';
 
   onButtonClick() {
     this.active = !this.active;
