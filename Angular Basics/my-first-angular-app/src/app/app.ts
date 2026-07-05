0fea3fb452f2b0511a529ddffc57531def2cee33
Mehdi <mehdicontacto@gmail.com>
2026-07-05 16:36:50 +0300

    Angular task 3 done

--- Angular Basics/my-first-angular-app/src/app/app.ts
+++ Angular Basics/my-first-angular-app/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './new-component/new-component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NewComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ Angular Basics/my-first-angular-app/src/app/new-component/new-component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-component',
+  imports: [],
+  templateUrl: './new-component.html',
+  styleUrl: './new-component.css',
+})
+export class NewComponent {
+  city = 'Helsinki';
+  temperature = '22°C';
+  condition = 'Sunny';
+}
